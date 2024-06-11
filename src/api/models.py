8096c5460f84fbdc80077caf85c7c96f7e4855cd
Héctor Chocobar-Torrejón <45291267+hchocobar@users.noreply.github.com>
2024-06-11 02:28:41 +0000

    fix: coding style according PEP8

--- src/api/models.py
+++ src/api/models.py
@@ -1,8 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 
+
 db = SQLAlchemy()
 
-class User(db.Model):
+
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
@@ -12,8 +14,6 @@ class User(db.Model):
         return f'<User {self.email}>'
 
     def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+        return {'id': self.id,
+                'email': self.email,
+                'is_active': self.is_active}
