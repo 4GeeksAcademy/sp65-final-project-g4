f7ea2ec5bbd31f289ed74a1a0055db760cfae26f
Marco Luque <101642586+marcoluqueh@users.noreply.github.com>
2024-06-26 18:25:58 +0000

    FEAT: Login

--- src/api/routes.py
+++ src/api/routes.py
@@ -289,6 +289,8 @@ def login():
     email = request.json.get("email", None)
     password = request.json.get("password", None)
     user = db.session.execute(db.select(Users).where(Users.email == email, Users.password == password, Users.is_active == True)).scalar()
+    print(user)
+    print(user.serialize())
     if user is None:
         return jsonify({"msg": "Wrong email"}) , 401
     if password != user.password:
@@ -299,7 +301,9 @@ def login():
                                                      'user_is_landlord': False,
                                                      'email': user.email})
         student = db.session.execute(db.select(Students).where(Students.id_user == user.id)).scalar()
-        serialized_data = {**user.serialize(), **student.serialize()}
+        print(student)
+        print(student.public_serialize())
+        serialized_data = {**user.serialize(), **student.public_serialize()}
         response_body['message'] = 'Student logged in'
         response_body['access_token'] = access_token
         response_body['data'] = serialized_data
@@ -310,10 +314,10 @@ def login():
                                                      'user_is_landlord': True,
                                                      'email': user.email})
         landlord = db.session.execute(db.select(Landlords).where(Landlords.id_user == user.id)).scalar()
-        serialized_data = {**user.serialize(), **landlord.serialize()}
+        """ serialized_data = {**user.serialize(), **landlord.serialize()} """
         response_body['message'] = 'Landlord logged in'
         response_body['access_token'] = access_token
-        response_body['data'] = serialized_data
+        """ response_body['data'] = serialized_data """
         return response_body, 200
 
 
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,7 +2,13 @@ const getState = ({getStore, getActions, setStore}) => {
 	return {
 		store: {
 			message: null,
-			demo: [{title: "FIRST", background: "white", initial: "white"}]
+			demo: [{title: "FIRST", background: "white", initial: "white"}],
+			apiContact: 'https://potential-robot-r5xxqjgwrggc5px9-3001.app.github.dev',
+			accessToken: null,
+			isLogedIn: false,
+			userEmail: "",
+			isAdmin: false,
+			userName: ""
 		},
 		actions: {
 			exampleFunction: () => {getActions().changeColor(0, "green");},  // Use getActions to call a function within a fuction
@@ -24,6 +30,37 @@ const getState = ({getStore, getActions, setStore}) => {
 					const data = await response.json()
 					setStore({ message: data.message })
 					return data;  // Don't forget to return something, that is how the async resolves
+			},
+			loginUser: async (userData) => {
+				const uri = getStore().apiContact + '/api/login'
+				const options = {
+					method: 'POST',
+					headers: {
+						'Content-type': 'application/json',
+					},
+					body: JSON.stringify(userData)
+				}
+				const response = await fetch(uri, options);
+				const data = await response.json();
+				const access_token = data.access_token;
+				setStore({accessToken: access_token})
+				setStore({userEmail: data.data.email})
+				setStore({userName: data.data.name})
+				localStorage.setItem('token', JSON.stringify(data.access_token))
+				localStorage.setItem('user', JSON.stringify(data.data))
+			},
+			logedIn: (userData) => {
+				console.log(userData)
+				setStore({isLogedIn: true, userEmail: userData.email})
+				console.log()
+
+			},
+			oldLogin: () => {
+				if (localStorage.getItem('token', 'user')) {
+					console.log('Hay usuario logeado', localStorage.getItem('user'))
+					getActions().logedIn(localStorage.getItem('user'))
+					setStore({accessToken: localStorage.getItem('token')})
+				} else {console.log('No hay usuario logeado')}
 			}
 		}
 	};
