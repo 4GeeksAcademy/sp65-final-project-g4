de18ce39e078e15fe76a8cada86f7af0218c8463
Pau Romeu Bruguera <pau.romeu.bruguera@gmail.com>
2024-07-09 22:37:53 +0000

    fix

--- src/front/js/pages/UploadRooms.jsx
+++ src/front/js/pages/UploadRooms.jsx
@@ -23,7 +23,7 @@ export const UploadRooms = () => {
 
     const handleSubmit = () => {
         console.log("Saving data...", formData);
-        actions.createNewRoom(formData);
+        createNewRoom(formData);
         
         const newRoom = store.rooms[(store.rooms.length - 1)];
 
@@ -37,6 +37,31 @@ export const UploadRooms = () => {
     const handleChange = (e) => {
         const { name, value } = e.target;
         setFormData({ ...formData, [name]: value });
+    };
+
+    const createNewRoom = async (dataToSend) => {
+        const url = `${process.env.BACKEND_URL}/api/rooms`;
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${store.accessToken}`					
+            },
+            body: JSON.stringify(dataToSend)
+        }
+
+        const response = await fetch(url, options);
+    
+        if (!response.ok) {
+            console.log("Error");
+            return;
+        }
+        const newRoom = await response.json();
+        if (newRoom.redirect) { 
+            navigate(`/roomprofile/${newRoom.redirect}`)
+         }
+        await actions.getRooms();
+        actions.createNewRoom(newRoom)
     };
 
     useEffect(() => {
