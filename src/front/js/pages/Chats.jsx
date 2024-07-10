9c8c9c4f46b9d06cc0976a13a9580c4a2d8881df
Victoria González Rodríguez <159072629+Vickygonrod@users.noreply.github.com>
2024-07-10 15:02:43 +0000

    chatStyle

--- src/front/js/pages/Chats.jsx
+++ src/front/js/pages/Chats.jsx
@@ -10,13 +10,13 @@ export const Chats = () => {
     const {store , actions} = useContext(Context)
 
 return (
-    <div className="container mt-5 pt-25"> 
+    <div className="container mt-5 pt-25 "> 
     <div className="row ">
-        <div className="col-lg-4 col-md-4 col-sm-12 ps-0 overflow-auto">
+        <div className="col-lg-4 col-md-4 col-sm-12 ps-0 chat-cont overflow-auto">
         <h3 className="red-color">{t('traduccion32')}</h3>
             <AllChats />        
         </div>
-        <div className="col-lg-8 col-md-8 col-sm-12 pr-0 ">
+        <div className="col-lg-8 col-md-8 col-sm-12 pr-0 overflow-auto">
 
         <h3 className="red-color"> Mensajes </h3>
        
